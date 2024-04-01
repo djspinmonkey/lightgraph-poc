4c9df8e565f2b817a236805620f40ed952c62b24
John Hyland <john.hyland@lightstep.com>
2024-04-01 13:31:35 -0700

    Add some additional fields from the base alert endpoint.

--- app/graphql/types/alert_type.rb
+++ app/graphql/types/alert_type.rb
@@ -2,10 +2,30 @@
 
 module Types
   class AlertType < Types::BaseObject
-    field :id, String, description: "The id of the alert"
+    field :id, ID, description: "The id of the alert"
 
     field :name, String, description: "The name of the alert"
 
-    field :queries, [String], description: "The query strings of any UQL queries found. This field is probably not useful as-is, since it mixes together all different kinds of alerts and queries. It is provided mostly as an example of doing some transformations of query data from the backend."
+    field :description, String, description: "A human readable description of the alert"
+
+    field :labels, [String], description: "A set of labels applied to this alert"
+
+    field :enable_no_data_alert, Boolean, "Indicates whether the alert will trigger if there is no data"
+
+    field :no_data_duration, Int, "How long in ms there can be no data before the alert triggers"
+
+    field :operand, String, "The operand of the alert expression"
+
+    field :warning_threshold, Float, "Values higher than this threshold put the alert into the warning state after a certain duration"
+
+    field :warning_threshold_duration, Int, "How many ms the values must be above the warning threshold before the alert enters the warning state"
+
+    field :critical_threshold, Float, "Values higher than this threshold put the alert into the critical state after a certain duration"
+
+    field :critical_threshold_duration, Int, "How many ms the values must be above the critical threshold before the alert enters the critical state"
+
+    field :is_multi_alert, Boolean, "Indicates whether this alert is a multi-alert"
+
+    field :queries, [String], description: "The query strings of any UQL queries found. This field may not be particularly useful as-is, since it mixes together all different kinds of alerts and queries. It is provided mostly as an example of doing some transformations of query data from the backend."
   end
 end
--- app/models/alert.rb
+++ app/models/alert.rb
@@ -17,8 +17,7 @@ class Alert
   end
 
   def queries
-    queries = []
-    queries = queries.union(base_attributes["metric-queries"]) if base_attributes.has_key?("metric-queries")
+    queries = base_attributes["metric-queries"] || []
     if base_attributes.has_key?("composite-alert")
       base_attributes.dig("composite-alert", "alerts").each do |alert|
         queries = queries.union(alert["queries"])
@@ -27,5 +26,20 @@ class Alert
 
     queries.collect { |q| q["query-string"] }
   end
+
+  # Attributes directly on base alert
+  def alerting_rules = base_attributes["alerting-rules"]
+  def description = base_attributes["description"]
+  def labels = base_attributes["labels"]
+
+  # Attributes on base alert expression
+  def enable_no_data_alert = base_attributes.dig("expression", "enable-no-data-alert")
+  def no_data_duration = base_attributes.dig("expression", "no-data-duration-ms")
+  def operand = base_attributes.dig("expression", "operand")
+  def warning_threshold = base_attributes.dig("expression", "thresholds", "warning")
+  def warning_threshold_duration = base_attributes.dig("expression", "thresholds", "warning-duration-ms")
+  def critical_threshold = base_attributes.dig("expression", "thresholds", "critical")
+  def critical_threshold_duration = base_attributes.dig("expression", "thresholds", "critical-duration-ms")
+  def is_multi_alert = base_attributes.dig("expression", "is-multi-alert")
 
 end
